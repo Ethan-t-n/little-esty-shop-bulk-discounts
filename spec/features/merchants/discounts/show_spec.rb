7316a4b8ce1a4f34eb1fc02cca5c027a27bb3f10
Ethan Nguyen <etnguyen16@gmail.com>
2022-08-06 22:38:53 -0600

    add edit discount link test

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe "merchant discounts show page", type: :feature do
         expect(page).to have_content("Percent Discount: 20%")
         expect(page).to have_content("Quantity Threshold: 10")
     end
+    it 'has a link to edit discount' do
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+
+        discount_1 = Discount.create!(percent: 20, quantity_threshold: 10, merchant_id: merchant_1.id)
+       
+        visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
+
+        click_button("Update Discount: #{discount_1.id}")
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts/#{discount_1.id}/edit")
+    end
 end
