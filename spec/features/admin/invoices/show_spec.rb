23e9c3af1122c0e94875c3fcc27b31dbb0d5850b
Ethan Nguyen <etnguyen16@gmail.com>
2022-08-09 15:23:39 -0600

    add test to show total and discounted total

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -122,4 +122,57 @@ RSpec.describe 'admin invoice show page' do
         expect(page).to_not have_content('Invoice status: completed')
         expect(page).to have_content('Invoice status: cancelled')
     end
+
+    it 'has shows the total both total and discounted for an invoice ' do
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+        merchant_2 = Merchant.create!(name: "Klein, Rempel and Jones", created_at: Time.now, updated_at: Time.now)
+
+        item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 3000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 4000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 5000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_4 = Item.create!(name: "Coat", description: "Perfect for a rainy day", unit_price: 5000, merchant_id: merchant_2.id, created_at: Time.now, updated_at: Time.now)
+        
+        customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+        
+        invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+        
+        invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_1.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_2.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 3, unit_price: item_3.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_4 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_4.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+
+        discount_1 = Discount.create!(percent: 10, quantity_threshold: 5, merchant_id: merchant_1.id)
+        discount_2 = Discount.create!(percent: 20, quantity_threshold: 10, merchant_id: merchant_1.id)
+        discount_3 = Discount.create!(percent: 20, quantity_threshold: 10, merchant_id: merchant_2.id)
+
+        visit "admin/invoices/#{invoice_1.id}"
+
+        expect(page).to have_content("Total Revenue Before Discounts: $1,200.00")
+        expect(page).to have_content("Total Revenue After Discounts: $1,005.00")
+     end 
+
+     it 'has shows the total both total and discounted for an invoice backup test ' do
+        merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+        merchant_2 = Merchant.create!(name: "Klein, Rempel and Jones", created_at: Time.now, updated_at: Time.now)
+
+        item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 3000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 4000, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+        item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 5000, merchant_id: merchant_2.id, created_at: Time.now, updated_at: Time.now)
+        
+        customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+        
+        invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+        
+        invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 12, unit_price: item_1.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_2.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+        invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_3.unit_price, status: 0, created_at: Time.now, updated_at: Time.now)
+
+        discount_1 = Discount.create!(percent: 20, quantity_threshold: 10, merchant_id: merchant_1.id)
+        discount_2 = Discount.create!(percent: 30, quantity_threshold: 15, merchant_id: merchant_1.id)
+
+        visit "admin/invoices/#{invoice_1.id}"
+
+        expect(page).to have_content("Total Revenue Before Discounts: $1,710.00")
+        expect(page).to have_content("Total Revenue After Discounts: $1,458.00")
+     end 
 end
